c28313b649993b4ee48ae3f6652ab2216bd5a508
GumiCatBro <mis084110@gmail.com>
2025-07-15 18:41:02 +0900

    firebase data test

--- app/src/main/java/com/example/snswithai/IntermediateActivity.kt
+++ app/src/main/java/com/example/snswithai/IntermediateActivity.kt
@@ -4,6 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import android.util.Log
 
 class IntermediateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,5 +29,23 @@ class IntermediateActivity : AppCompatActivity() {
             startActivity(intent)
             finish() // Optional: finish IntermediateActivity so user can't go back to it
         }
+
+        // Firebase Database logging
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.reference
+
+        myRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.value
+                Log.d("DBTEST", "Firebase Data: $value")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("DBTEST", "Failed to read value.", error.toException())
+            }
+        })
     }
 }
